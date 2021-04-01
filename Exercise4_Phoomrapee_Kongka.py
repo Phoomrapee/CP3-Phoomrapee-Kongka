a510da673be6ca0507f52dce5e69f481a1faa6db
Phoomrapee <81746199+Phoomrapee@users.noreply.github.com>
2021-04-01 16:16:40 +0700

    Create Exercise4_Phoomrapee_Kongka.py

---
+++ Exercise4_Phoomrapee_Kongka.py
@@ -0,0 +1,9 @@
+FD_ENG = 90
+GN_BUS = 75.25
+ICS    = 80
+CP     = 60
+print("----Your Score----")
+print("Foundation English             "," : ",FD_ENG)
+print("Generall Business              "," : ",GN_BUS)
+print("Introdution to Computer Systems"," : ",ICS)
+print("Computer Prohgramming          "," : ",CP ) 
